e48eefc6f39aa3b65dd127d47febbb08d29a0964
Robin Joseph <robinjoseph08@sbcglobal.net>
2013-08-19 21:24:36 -0700

    start adding structure to the site with tutorials/categories/lesson

--- web.js
+++ web.js
@@ -29,6 +29,7 @@ packer({
     __dirname + '/stylesheets/style.css',
     __dirname + '/stylesheets/index.css',
     __dirname + '/stylesheets/header.css',
+    __dirname + '/stylesheets/tutorials.css',
   ],
   output: __dirname + '/stylesheets/style.min.css',
   callback: function ( err, code ){
@@ -78,29 +79,56 @@ var db = mongo.connect(databaseUrl, collections);
 /***              ROUTE              ***/
 /***************************************/
 
+  // db.posts.save({
+  //  title: "This is the second title",
+  //  body: "This is the second body.",
+  //  created_at: new Date().getTime(),
+  //  liked: 0
+  // }, function(err, saved) {
+  //   if(err || !saved)
+  //    console.log("Post not saved");
+  //   else
+  //    console.log("Post saved");
+  //    console.log(saved);
+  // });
+
 app.get('/', function (req, res) {
-	console.log('GET /');
-	// db.posts.save({
-	// 	title: "This is the second title",
-	// 	body: "This is the second body.",
-	// 	created_at: new Date().getTime(),
-	// 	liked: 0
-	// }, function(err, saved) {
-	//   if(err || !saved)
-	//   	console.log("Post not saved");
-	//   else
-	//   	console.log("Post saved");
-	//   	console.log(saved);
-	// });
+  console.log('GET /');
   res.render('index', {});
 });
 
-app.get('/admin', function (req, res) {
-	console.log('GET /admin');
-  res.render('admin_login', {
-  });
+app.get('/tutorials', function (req, res) {
+  console.log('GET /tutorials');
+  res.render('tutorials/index', {});
 });
 
+app.get('/tutorials/:category', function (req, res) {
+  console.log('GET /tutorials/' + req.params['category']);
+  res.render('tutorials/' + req.params['category'] + '/index', {});
+});
+
+app.get('/tutorials/:category/:id', function (req, res) {
+  console.log('GET /tutorials/' + req.params['category'] + '/' + req.params['id']);
+  res.render('tutorials/' + req.params['category'] + '/' + req.params['id'], {});
+});
+
+app.get('/blog', function (req, res) {
+  console.log('GET /tutorials');
+  res.render('blog', {});
+});
+
+app.get('/about', function (req, res) {
+  console.log('GET /about');
+  res.render('about', {});
+});
+
+app.post('/requests', function (req, res) {
+  console.log('POST /requests');
+  console.log(req.body.data);
+  res.json(200, { data: 'got em'});
+});
+
+// Compiled Assets
 app.get('/style.min.css', function (req, res) {
   console.log('GET /style.min.css');
   res.sendfile(__dirname + '/stylesheets/style.min.css');
@@ -112,45 +140,22 @@ app.get('/scripts.min.css', function (req, res) {
 });
 
 // Amble Font
-app.get('/amble-regular-webfont.eot', function (req, res) {
-  console.log('GET /amble-regular-webfont.eot');
-  res.sendfile(__dirname + '/fonts/amble-regular-webfont.eot');
-});
-
-app.get('/amble-regular-webfont.woff', function (req, res) {
-  console.log('GET /amble-regular-webfont.woff');
-  res.sendfile(__dirname + '/fonts/amble-regular-webfont.woff');
-});
-
-app.get('/amble-regular-webfont.ttf', function (req, res) {
-  console.log('GET /amble-regular-webfont.ttf');
-  res.sendfile(__dirname + '/fonts/amble-regular-webfont.ttf');
-});
-
-app.get('/amble-regular-webfont.svg', function (req, res) {
-  console.log('GET /amble-regular-webfont.svg');
-  res.sendfile(__dirname + '/fonts/amble-regular-webfont.svg');
+app.get('/amble-regular-webfont.(eot|woff|ttf|svg)', function (req, res) {
+  console.log('GET /amble-regular-webfont.' + req.params[0]);
+  res.sendfile(__dirname + '/fonts/amble-regular-webfont.' + req.params[0]);
 });
 
 // Source Code Pro Font
-app.get('/sourcecodepro-regular-webfont.eot', function (req, res) {
-  console.log('GET /sourcecodepro-regular-webfont.eot');
-  res.sendfile(__dirname + '/fonts/sourcecodepro-regular-webfont.eot');
+app.get('/sourcecodepro-regular-webfont.(eot|woff|ttf|svg)', function (req, res) {
+  console.log('GET /sourcecodepro-regular-webfont.' + req.params[0]);
+  res.sendfile(__dirname + '/fonts/sourcecodepro-regular-webfont.' + req.params[0]);
 });
 
-app.get('/sourcecodepro-regular-webfont.woff', function (req, res) {
-  console.log('GET /sourcecodepro-regular-webfont.woff');
-  res.sendfile(__dirname + '/fonts/sourcecodepro-regular-webfont.woff');
-});
-
-app.get('/sourcecodepro-regular-webfont.ttf', function (req, res) {
-  console.log('GET /sourcecodepro-regular-webfont.ttf');
-  res.sendfile(__dirname + '/fonts/sourcecodepro-regular-webfont.ttf');
-});
-
-app.get('/sourcecodepro-regular-webfont.svg', function (req, res) {
-  console.log('GET /sourcecodepro-regular-webfont.svg');
-  res.sendfile(__dirname + '/fonts/sourcecodepro-regular-webfont.svg');
+// Catch All For 404
+app.get('(*)', function (req, res) {
+  console.log('GET ' + req.params[0]);
+  console.log('404');
+  res.render('404', {});
 });
 
 /***************************************/
@@ -170,6 +175,7 @@ io.sockets.on('connection', function(socket) {
   bash.stdin.write('cd\n');
   var state = 3;
   bash.stdin.write('echo $HOME\n');
+  // listen for when the bash shell returns something
   bash.stdout.on('data', function(data) {
     switch(state) {
       case 0: // normal cmds
@@ -208,19 +214,23 @@ io.sockets.on('connection', function(socket) {
         break;
     }
   });
+  // enetered command
   socket.on('enter', function(data) {
     var _cmd = data.cmd.replace(/&gt;/g,'>').replace(/&lt;/g,'<').replace(/&amp;/g,'&');
     console.log(_cmd);
-    if(new RegExp('^su( |$)|^sudo( |$)').test(_cmd)) {
+    if(new RegExp('(^ *| *\| *| *&& *| *; *)su( |$)|(^ *| *\| *| *&& *| *; *(then)* *)sudo( |$|;|&&)','g').test(_cmd)) {
       bash.stdin.write('echo \'~~~ No root for you! ~~~\'\n');
+    } else if(new RegExp('(^ *| *\| *| *&& *| *; *)rm( |$)|(^ *| *\| *| *&& *| *; *(then)* *)srm( |$|;|&&)','g').test(_cmd)) {
+      bash.stdin.write('echo \'~~~ You probably shouldn\'\'t be deleting things you don\'\'t know about... ~~~\'\n');
     } else {
       bash.stdin.write(_cmd + '\n');
     }
-    if(new RegExp('^cd|^$').test(_cmd)) {
+    if(new RegExp('(^ *| *\| *| *&& *| *; *(then)* *)cd *[^|]*$|^ *$|>','g').test(_cmd)) {
       state = 1;
       bash.stdin.write('pwd\n');
     }
   });
+  // tab completion
   socket.on('tab', function(data) {
     var _cmd = data.cmd.replace(/&gt;/g,'>').replace(/&lt;/g,'<').replace(/&amp;/g,'&');
     var cur = '', cmd;
@@ -236,5 +246,9 @@ io.sockets.on('connection', function(socket) {
     }
     state = 2;
     bash.stdin.write('ls ' + cur + ' | egrep ^' + cmd + '\n');
+  });
+
+  socket.on('disconnect', function () {
+    bash.stdin.write('exit\n');
   });
 });
